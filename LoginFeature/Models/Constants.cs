f0e2f6b301959ee5cb4191d49182a18ad5da937b
NeilByrne97 <45207307+NeilByrne97@users.noreply.github.com>
2019-03-06 16:48:56 +0000

    Add files via upload
    Basic login which checks user pin and password to hard coded users. This will be changed to just user pin as there is no real need for a password in a quick login till system.

---
+++ LoginFeature/Models/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoginFeature.Models
+{
+    public class Constants
+    {
+        public static bool isDev = true;
+    }
+}
---
+++ LoginFeature/Views/LoginPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using LoginFeature.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginFeature.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+
+            if (user.CheckInformation())
+            {
+                DisplayAlert("Login", "Login Success, empty", "OK");
+            }
+            else
+            {
+                DisplayAlert("Login", "Login Not Correct, Empty Username or Password", "OK");
+            }
+
+        }
+
+
+	}
+}
