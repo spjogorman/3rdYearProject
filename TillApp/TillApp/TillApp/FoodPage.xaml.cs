2b781b20e3139532a91eb6c1960c2ad125509120
NeilByrne97 <45207307+NeilByrne97@users.noreply.github.com>
2019-03-27 19:05:23 +0000

    More functional user input for FoodPage
    Modified code from Xamarin Forms of a Calculator to retrieve and display user input from number buttons.

--- TillApp/TillApp/TillApp/FoodPage.xaml.cs
+++ TillApp/TillApp/TillApp/FoodPage.xaml.cs
@@ -12,9 +12,13 @@ namespace TillApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class FoodPage : ContentPage
 	{
+	    	int currentState = 1;
+        	double firstNumber, secondNumber;
+		
 		public FoodPage ()
 		{
 			InitializeComponent ();
+			OnClear(this, null);
 		}
 
         private void Button_OnClicked(object sender, EventArgs e)
@@ -26,5 +30,46 @@ namespace TillApp
         {
 
         }
+	
+	// Number Button Functionality
+        void OnSelectNum(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;   // string of numbers user inputs
+
+           
+            if (this.userInput.Text == "0" || currentState < 0) // currentState initalised as 1
+            {
+                this.userInput.Text = "";
+                if (currentState < 0)
+                    currentState *= -1; // < 0
+            }
+
+            this.userInput.Text += pressed;
+
+            double number;
+            if (double.TryParse(this.userInput.Text, out number))
+            {
+                this.userInput.Text = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+            }
+        }
+
+        // Clears input
+        void OnClear(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            this.userInput.Text = "0";
+        }
+	
     }
 }
